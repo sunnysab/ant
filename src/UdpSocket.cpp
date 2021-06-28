02f0c3c535213af1293404c959773ef355496374
sunnysab <sunnysab@yeah.net>
2021-06-28 13:47:32 +0800

    更新 UdpSocket 类代码, 优化代码
    1. 更新部分代码语法规范
    2. 重命名部分变量, 并对函数参数进行微调
    3. 删除一些不必要的代码

--- src/UdpSocket.cpp
+++ src/UdpSocket.cpp
@@ -5,74 +5,69 @@
 #include <stdexcept>
 #include "UdpSocket.h"
 
-UdpSocket::UdpSocket(const std::string &local_address, unsigned short port) {
-    this();
 
+UdpSocket::UdpSocket(const std::string &local_address, port_t port)
+        : UdpSocket() {
+    // Bind with the address in parameter.
+    this->bind(local_address, port);
 }
 
 UdpSocket::UdpSocket() {
     this->s_ = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    if (this->s_ == INVALID_SOCKET) {
+    if (INVALID_SOCKET == this->s_) {
         throw std::exception("Error occurred while calling socket().");
     }
 }
 
-bool UdpSocket::bind(const std::string &local_address, unsigned short port) {
-    sockaddr_in  addr;
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(port);
-    addr.sin_addr.S_un.S_addr = inet_addr(local_address.c_str());
-    if (::bind(this->s_, (sockaddr *)&addr, sizeof(addr)) == SOCKET_ERROR) {
+bool UdpSocket::bind(const std::string &local_address, port_t port) {
+    this->local_addr_ = sockaddr_in{
+            .sin_family = AF_INET,
+            .sin_port = htons(port),
+            .sin_addr.S_un.S_addr = inet_addr(local_address.c_str()),
+    };
+    if (::bind(this->s_, (sockaddr *) &this->local_addr_, sizeof(sockaddr_in)) == SOCKET_ERROR) {
         throw std::exception("Error occurred while calling bind().");
     }
     return true;
 }
 
-size_t UdpSocket::sendto(const vector<char> &buffer, const std::string &remote_address, unsigned short port) {
-    sockaddr_in remoteAddr;
-    remoteAddr.sin_family=AF_INET;
-    remoteAddr.sin_port=port;
-    remoteAddr.sin_addr.S_un.S_addr= inet_addr(remote_address.c_str());
-    int ret = ::sendto(this->s_,&buffer[0],buffer.size(),0,(sockaddr *)&remoteAddr,sizeof(remoteAddr));
-    if (ret == -1) {
-        throw std::exception ("Error occurred while calling sendto().");
-    }
-    return ret;
-}
-
-size_t UdpSocket::send(const vector<char> &buffer) {
-    int ret = ::sendto(this->s_,&buffer[0],buffer.size(),0,(sockaddr *)&(this->remote_addr_),sizeof(this->remote_addr_));
-    if (ret == -1) {
-        throw std::exception ("Error occurred while calling sendto().");
-    }
-    return ret;
-}
-
-void UdpSocket::connect(const std::string &remote_address, unsigned short port) {
-    this->remote_addr_.sin_family=AF_INET;
-    this->remote_addr_.sin_port=port;
-    this->remote_addr_.sin_addr.S_un.S_addr=inet_addr(remote_address.c_str());
-}
-
-size_t UdpSocket::recvfrom(vector<char> &buffer) {
-
-    int ret = ::recvfrom(this->s_,&buffer[0],buffer.size(),0,(sockaddr *)&(this->remote_addr_,&(sizeof(this->remote_addr_)))
-    if (ret<=0) {
+size_t UdpSocket::sendto(const vector <byte> &buffer, const std::string &remote_address, port_t remote_port) {
+    sockaddr_in dest_addr{
+            .sin_family = AF_INET,
+            .sin_port = remote_port,
+            .sin_addr.S_un.S_addr = inet_addr(remote_address.c_str()),
+    };
+    int sent_size = ::sendto(this->s_, &buffer[0], buffer.size(), 0, (sockaddr *) &dest_addr, sizeof(dest_addr));
+    if (sent_size == -1) {
         throw std::exception("Error occurred while calling sendto().");
-        return ret;
     }
-    return ret;
+    return sent_size;
 }
 
-size_t UdpSocket::recvfrom(vector<char> &buffer, const std::string &remote_address, unsigned short port) {
-    this->remote_addr_.sin_family=AF_INET;
-    this->remote_addr_.sin_port=port;
-    this->remote_addr_.sin_addr.S_un.S_addr=inet_addr(remote_address.c_str());
-    int ret = ::recvfrom(this->s_,&buffer[0],buffer.size(),0,(sockaddr *)&(this->remote_addr_,&(sizeof(this->remote_addr_)))
-    if (ret<=0) {
+size_t UdpSocket::send(const vector <byte> &buffer) {
+    int sent_size = ::sendto(this->s_, &buffer[0], buffer.size(), 0, (sockaddr *) &(this->remote_addr_)
+                             , sizeof(sockaddr_in));
+    if (sent_size == -1) {
         throw std::exception("Error occurred while calling sendto().");
-        return ret;
     }
-    return ret;
+    return sent_size;
 }
 
+void UdpSocket::connect(const std::string &remote_address, port_t remote_port) {
+    this->remote_addr_ = sockaddr_in{
+            .sin_family = AF_INET,
+            .sin_port = htons(remote_port),
+            .sin_addr.S_un.S_addr = inet_addr(remote_address.c_str()),
+    };
+}
+
+size_t UdpSocket::recvfrom(vector <byte> &buffer, const std::string &remote_address, port_t &port) {
+    sockaddr_in  source_addr {};
+
+    int recv_size = ::recvfrom(this->s_, &buffer[0], buffer.size(), 0
+                         , (sockaddr *) &source_addr, sizeof(sockaddr_in));
+    if (recv_size <= 0) {
+        throw std::exception("Error occurred while calling recvfrom().");
+    }
+    return recv_size;
+}
--- src/UdpSocket.h
+++ src/UdpSocket.h
@@ -4,22 +4,45 @@
 #include <iostream>
 #include <winsock2.h>
 
+using byte = unsigned char;
+using port_t = unsigned short;
 
-class UdpSocket
-{
+
+class UdpSocket {
+public:
+    constexpr DefaultBufferSize = 1500;
+
 private:
-    SOCKET  s_ = INVALID_SOCKET;
-    sockaddr_in  remote_addr_ {};
+    // Windows socket handle
+    SOCKET s_ = INVALID_SOCKET;
+
+    // Local address and port
+    sockaddr_in local_addr_ {};
+    // Set default remote address and port
+    sockaddr_in remote_addr_{};
 
 public:
+    // Construct an Udp socket without local address.
     UdpSocket();
-    UdpSocket(const std::string  &remote_address, unsigned short  port);
-    void  connect(const std::string  &local_address, unsigned short port);
-    bool  bind(const std::string  &local_address, unsigned short port);
-    size_t  sendto(const vector<char> &buffer, const std::string &remote_address, unsigned short port);
-    size_t  send(const vector<char> &buffer);
-    size_t  recvfrom(vector<char> &buffer, const std::string &remote_address, unsigned short port);
-    size_t  recvfrom(vector<char> &buffer)
+
+    // Construct an Udp socket with local address by bind() function.
+    UdpSocket(const std::string &remote_address, port_t port);
+
+    // Set remote host and port, to avoid filling address each call.
+    void connect(const std::string &local_address, port_t port);
+
+    // Bind local address and port. */
+    bool bind(const std::string &local_address, port_t port);
+
+    // Send datagram to the given address and port.
+    size_t sendto(const vector <byte> &buffer, const std::string &remote_address, port_t remote_port);
+
+    // Send datagram to the set address and port.
+    size_t send(const vector <byte> &buffer);
+
+    // Block and wait an Udp packet in,
+    // and then set source address to `remote_address`, set source port to `port`. */
+    size_t recvfrom(vector <byte> &buffer, const std::string &remote_address, port_t port);
 
 };
 
