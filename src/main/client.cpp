8c32bdac4503a823cb05d9626dd8e947c70ab199
sunnysab <sunnysab@yeah.net>
2021-06-29 16:08:30 +0800

    删除 client.cpp, server.cpp 及调试用代码

---
+++ src/main/client.cpp
@@ -0,0 +1,10 @@
+//
+// Created by sunnysab on 2021/6/29.
+//
+
+#include "AntClient.h"
+
+int main(int argc, char *argv[]) {
+    AntClient client;
+    return 0;
+}
---
+++ src/main/server.cpp
@@ -0,0 +1,12 @@
+//
+// Created by sunnysab on 2021/6/28.
+//
+
+#include "AntServer.h"
+
+
+int main(int argc, char *argv[]) {
+    AntServer server;
+
+    return 0;
+}
