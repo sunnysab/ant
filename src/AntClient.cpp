20c0274da6454f913c567221bbd4fe3907df5cea
heyu <416394356@qq.com>
2021-06-30 19:27:31 +0800

    修改 AntClient 类，加入基本功能
    但未进行测试，代码也未完成

--- src/AntClient.cpp
+++ src/AntClient.cpp
@@ -1,5 +1,37 @@
 //
 // Created by sunnysab on 2021/6/23.
 //
-
+#include <iostream>
+#include <fstream>
 #include "AntClient.h"
+#include "UdpSocket.h"
+
+
+AntClient::AntClient(const std::string &local_addr, const port_t port) {
+    this->bind(local_addr, port);
+}
+
+void AntClient::bind(const std::string &local_address, port_t port) {
+    this->socket_.bind(local_address, port);
+}
+
+void AntClient::open() {
+    this->input_file_.open(this->file_path_, std::ios::in);
+}
+
+void AntClient::connect(const std::string &remote_address, port_t port) {
+    this->socket_.connect(remote_address, port);
+}
+
+void AntClient::send() {
+    this->socket_.send(this->input_file_);
+}
+
+void AntClient::set_block_size() {
+
+}
+
+void AntClient::get_block_size() {
+
+}
+
--- src/AntClient.h
+++ src/AntClient.h
@@ -2,14 +2,52 @@
 // Created by sunnysab on 2021/6/23.
 //
 
-#ifndef ANT_ANTCLIENT_H
-#define ANT_ANTCLIENT_H
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include "UdpSocket.h"
 
 
-class AntClient
-{
+class AntClient {
+protected:
+    // Udp socket handle
+    UdpSocket socket_;
+    // File path
+    std::string file_path_;
+    // Block size
+    size_t block_size;
 
+//protected:
+//
+//    // Try send file properties
+//    void send_file_properties();
+//
+//    // Try send data
+//    void send_file_content();
+
+public:
+    // Construct an Udp socket without local address.
+    AntClient() = default;
+
+    // Construct an Udp socket with local address by bind() function.
+    AntClient(const std::string &local_addr, const port_t port);
+
+    // Bind local address and port
+    void bind(const std::string &local_address, port_t port);
+
+    //Create file
+    void open();
+
+    //connect server
+    void connect(const std::string &remote_address, port_t port);
+
+    //transfer protected send function
+    void send();
+
+    //set file block
+    void set_block_size();
+
+    //get size of data or file
+    void get_block_size();
 };
-
-
-#endif //ANT_ANTCLIENT_H
